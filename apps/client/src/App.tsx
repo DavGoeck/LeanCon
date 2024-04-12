abaf5cd98c88430cf96ed59cff3a909db3ccd0a2
David Göckede <david.goeckede@protonmail.com>
2024-04-12 16:28:02 +0200

    Introduce react-router and implement fallback page

--- apps/client/src/App.tsx
+++ apps/client/src/App.tsx
@@ -1,7 +1,23 @@
 import { useEffect, useState } from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage/>
+  },
+  {
+    path: "*",
+    element: <NotFound />
+  }
+])
+
 function App() {
+  return <RouterProvider router={router}/>
+}
+
+function HomePage() {
   const [greeting, setGreeting] = useState('')
 
   useEffect(() => {
@@ -13,6 +29,10 @@ function App() {
       <h1>{greeting}</h1>
     </>
   )
+}
+
+function NotFound() {
+  return <div>404 Not Found</div>;
 }
 
 export default App
